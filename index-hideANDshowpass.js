efbf9680818b93ea9410c4a86ad31b2a3a3d3fa5
Abdullah <abdulbrothers10@gmail.com>
2024-12-20 09:47:22 +0500

    hide and show password

---
+++ index-hideANDshowpass.js
@@ -0,0 +1,24 @@
+// let eyeicon = document.querySelector('.eyeicon');
+// let password = document.querySelector('.password');
+// const body = document.body;
+
+
+
+// eyeicon.onclick = function(){
+//     if(password.type == "password"){
+//         password.type == "text";
+//         eyeicon.src = "https://cdn-icons-png.flaticon.com/128/3423/3423234.png"
+//         }else{
+//             password.type == "password";
+//             eyeicon.src = "https://cdn-icons-png.flaticon.com/128/8276/8276554.png"
+//         }
+// }
+
+// // eyeIcon.addEventListener('click',function(){
+// //     if(password.type == "password"){
+// //         password.type == "text";
+// //         }else{
+// //             password.type == "password";
+// //         }
+// // })
+
